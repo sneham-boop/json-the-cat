e5e5367b8c3998d9d1a1a6a35e77c9b970b84343
Sneha Mahajan <sneh.km@gmail.com>
2022-04-21 16:35:16 -0400

    Clean code after talking to mentor

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,14 +5,19 @@ const breed = args[0];
 const PATH = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 request(PATH, (error, response, body) => {
-  console.log("error:", error); // Print the error
-  console.log("statusCode:", response && response.statusCode); // Print status code
-
-  // Check for valid breed
-  try {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  } catch {
-    console.log("I cannot find this breed right now");
+  // Print the error
+  if (error) {
+    return console.log("error:", error);
   }
+
+  // Parse data from received body
+  const data = JSON.parse(body);
+
+  // Check for a valid breed
+  const breedObject = data[0];
+  if (!breedObject) {
+    return console.log("Breed not found!!!", breed);
+  }
+
+  console.log(breedObject.description);
 });
